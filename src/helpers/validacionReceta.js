b53aef56b06f0d847b3f4cdd9b7ff7f72d65f57f
franAndrad <franciscoezequielandrade@gmail.com>
2022-09-19 17:49:40 -0300

    se agrego la validacion de los ingredientes

--- src/helpers/validacionReceta.js
+++ src/helpers/validacionReceta.js
@@ -14,6 +14,20 @@ const validarReceta = [
     check('imagen', 'La url de la imagen es invalida')
         .isLength({min:0,max:500})
         .withMessage('La url debe contener menos de 500 caracteres'),
+    check('ingredientes')
+        .custom((input)=>{
+            let verdaderos = 0;
+            input.forEach(element => {
+                if (element.length >= 2 && element.length <= 50)
+                    verdaderos++;
+            });
+            if (input.length === verdaderos && input.length > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        })
+        .withMessage('Debe contener al menos un ingrediente y estos deben estar entre 2 a 60 caracteres'),
     (req, res, next) => {
         resultadosValidacion(req, res, next);
     },
